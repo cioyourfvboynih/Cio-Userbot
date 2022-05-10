3843cf2e9418382316eee94b18f327209160c6c0
KingCio <103745710+cioyourfvboynih@users.noreply.github.com>
2022-05-10 08:30:03 +0700

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -122,7 +122,7 @@ async def autobot():
             await asyncio.sleep(1)
             await bot.send_message(bf, f"@{username}")
             await asyncio.sleep(1)
-            await bot.send_file(bf, "userbot/resources/https://telegra.ph/file/81858c3a4428f97fec321.jpg")
+            await bot.send_file(bf, "resources/IMG_20220425_001016_896.jpg")
             await asyncio.sleep(3)
             await bot.send_message(bf, "/setabouttext")
             await asyncio.sleep(1)
@@ -135,7 +135,7 @@ async def autobot():
             await bot.send_message(bf, f"@{username}")
             await asyncio.sleep(1)
             await bot.send_message(
-                bf, f"🔥 Owner ~ {who.first_name} 🔥\n\n🔥 Powered By ~ @ciomusic\n\n❄️ Powered By ~ @projectcio"
+                bf, f"🔥 Owner ~ {who.first_name} 🔥\n\n🔥 Powered By ~ @ciomusic\n\n, 🔥Powered By ~ @projectcio"
             )
             await bot.send_message(
                 BOTLOG_CHATID,
@@ -156,7 +156,7 @@ async def autobot():
                              manage_call=True,
                          )
             await bot(EditAdminRequest(int(BOTLOG_CHATID), f"@{username}", rights, "ᴀssɪsᴛᴀɴᴛ  ᴄɪᴏ")) 
-            ppk = "userbot/resources/https://telegra.ph/file/81858c3a4428f97fec321.jpg"
+            ppk = "resources/IMG_20220425_001016_896.jpg"
             await bot(EditPhotoRequest(BOTLOG_CHATID, await bot.upload_file(ppk)))
             heroku_var["BOT_TOKEN"] = token
             heroku_var["BOT_USERNAME"] = f"@{username}"
@@ -177,7 +177,7 @@ async def autobot():
         await asyncio.sleep(1)
         await bot.send_message(bf, f"@{username}")
         await asyncio.sleep(1)
-        await bot.send_file(bf, "userbot/resources/https://telegra.ph/file/81858c3a4428f97fec321.jpg")
+        await bot.send_file(bf, "resources/IMG_20220425_001016_896.jpg")
         await asyncio.sleep(3)
         await bot.send_message(bf, "/setabouttext")
         await asyncio.sleep(1)
@@ -211,7 +211,7 @@ async def autobot():
                  manage_call=True,
              )
         await bot(EditAdminRequest(int(BOTLOG_CHATID), f"@{username}", rights, "ᴀssɪsᴛᴀɴᴛ  ᴄɪᴏ"))
-        ppk = "userbot/resources/https://telegra.ph/file/81858c3a4428f97fec321.jpg"
+        ppk = "resources/IMG_20220425_001016_896.jpg"
         await bot(EditPhotoRequest(BOTLOG_CHATID, await bot.upload_file(ppk)))
         heroku_var["BOT_TOKEN"] = token
         heroku_var["BOT_USERNAME"] = f"@{username}"
